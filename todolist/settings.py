cffb3006f770045a04bb2c7931a85aad5fae9530
Дмитрий С <112376316+DSvistelnik@users.noreply.github.com>
2023-03-26 15:32:14 +0300

    docker-compose.yaml

--- todolist/settings.py
+++ todolist/settings.py
@@ -87,10 +87,12 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'postgres',
-        'PASSWORD': 'postgres',
-
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('POSTGRES_DB'),
+        'USER': env.str('POSTGRES_USER'),
+        'PASSWORD': env.str('POSTGRES_PASSWORD'),
+        'HOST': env.str('POSTGRES_HOST', default='127.0.0.1'),
+        'PORT': env.int('POSTGRES_PORT', default=5432),
     }
 }
 
